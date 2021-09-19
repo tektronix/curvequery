cc553c92001f1d36ea949240a3d5163f2dd8355b
cstryker <chad.stryker@tektronix.com>
2021-09-19 05:12:58 -0700

    Cleaned up digital test code to improve readability

--- test/test_curve_digital.py
+++ test/test_curve_digital.py
@@ -1,5 +1,8 @@
 import pytest
-from time import sleep
+
+FREQ_D0 = 625e3
+MASK_D7_CLOCK = 0x80
+MASK_D6_to_D0_DATA = 0x7F
 
 
 @pytest.fixture(scope="session")
@@ -39,8 +42,8 @@ def decode_digital_data_counter(waveform):
 
         # Bit 7 is the clock, bits 6:0 make up the data value
         # Capture the new 7-bit value on the falling edge of the clock
-        if word & 0x80 < next_word & 0x80:
-            result.append(next_word & 0x7F)
+        if word & MASK_D7_CLOCK < next_word & MASK_D7_CLOCK:
+            result.append(next_word & MASK_D6_to_D0_DATA)
 
     return result
 
@@ -72,22 +75,27 @@ def test_counter_check_sources(curve_data_dch_counter, target):
 
 def test_counter_incrementing_value(curve_data_dch_counter):
     """Verify the resulting waveforms capture a incrementing count"""
-    wave_in = curve_data_dch_counter.data["CH2"].data
-    counts = decode_digital_data_counter(wave_in)
+    wave_data = curve_data_dch_counter.data["CH2"].data
+    counts = decode_digital_data_counter(wave_data)
     for i, code in enumerate(counts):
         try:
             next_code = counts[i + 1]
         except IndexError:
             break
-        assert (code + 1) % 0x80 == next_code
+        assert (code + 1) % (MASK_D6_to_D0_DATA + 1) == next_code
 
 
 def test_counter_decomposed_frequency(curve_data_dch_counter_decompose):
     """Test relative frequencies of the decomposed bits"""
     for i in range(7):
-        wave_in = curve_data_dch_counter_decompose.data[f"CH2_D{i}"].data
-        bitstream = decode_digital_bitstream(wave_in)
-        expected = 250 / 2 ** i
+        wave_data = curve_data_dch_counter_decompose.data[f"CH2_D{i}"].data
+        wave_duration = curve_data_dch_counter_decompose.data[
+            f"CH2_D{i}"
+        ].x_scale.slope * len(wave_data)
+        half_cycles_d0 = 2 * FREQ_D0 * wave_duration
+        bitstream = decode_digital_bitstream(wave_data)
+        expected = half_cycles_d0 / 2 ** i
+        print(i)
         assert len(bitstream) + 2 > expected
         assert len(bitstream) - 2 < expected
 
